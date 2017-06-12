6e39771fbfa65728a8d316bf4d21ad1f31b45794
ellanos <ellanos@openmailbox.org>
2017-06-12 03:22:24 -0500

    Create entrypoint.sh
    Adding entrypoint for local user creation.

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Add local user
+# Either use the LOCAL_USER_ID if passed in at runtime or
+# fallback
+
+USER_ID=${LOCAL_USER_ID:-10001}
+
+echo "Starting with UID : $USER_ID"
+useradd --shell /bin/bash -u $USER_ID -o -c "" -m ulocal
+export HOME=/home/ulocal
+
+exec /usr/local/bin/gosu ulocal "$@"
